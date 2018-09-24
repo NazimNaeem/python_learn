4f032437d5f6b833fec6c0004b5846ceb479b338
nazimnaeem <nazim.naeem@ebryx.com>
2018-09-24 10:59:36 +0500

    task16_v1

--- task16/main2.py
+++ task16/main2.py
@@ -10,7 +10,7 @@ driver.get("https://www.applebees.com/en/locations")
 
 elem = driver.find_element_by_id("location-search-input")
 
-elem.send_keys("lahore" + Keys.RETURN)
-
+elem.send_keys("Gujrat,India" + Keys.RETURN)
+time.sleep(3)
 elem = driver.find_element_by_class_name("c-location-input__action").click()
 
